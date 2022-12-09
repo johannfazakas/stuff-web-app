c7dff8507ce1d66cb448ae717cfb3c6598943858
Johann Fazakas <johannfazakas@gmail.com>
2022-12-09 17:01:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# sd-web-app
+Web App for Social Diary
+
+## Build & Run
+
+### Local development
+`./gradlew clean run --continuous`
---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    kotlin("js") version "1.7.10"
+    kotlin("plugin.serialization") version "1.7.10"
+}
+
+group = "ro.jf.sd"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    js {
+        browser {
+            commonWebpackConfig {
+                cssSupport.enabled = true
+            }
+        }
+        binaries.executable()
+    }
+}
+
+dependencies {
+    //React, React DOM + Wrappers
+    implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:1.0.0-pre.354"))
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom")
+
+    //Coroutines & serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+}
+
+rootProject.extensions.configure<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension> {
+    versions.webpackCli.version = "4.10.0"
+}
