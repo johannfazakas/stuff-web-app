f584b9c907cd96693add65613367eb35171271c9
Johann Fazakas <johannfazakas@gmail.com>
2023-01-18 12:38:18 +0200

    render hello message with kotlin html dsl

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,15 @@
 import kotlinx.browser.document
+import react.Fragment
+import react.create
+import react.dom.client.createRoot
+import react.dom.html.ReactHTML.h1
 
 fun main() {
-    document.bgColor = "green"
+    document.bgColor = "orange"
+    val container = document.getElementById("root") ?: error("Couldn't find root container!")
+    createRoot(container).render(Fragment.create {
+        h1 {
+            +"Welcome to the Social Diary! This is a React+Kotlin/JS app!"
+        }
+    })
 }
