e81e939a65e28bb55362aed61ca806f33c6458cb
Johann <johannfazakas@gmail.com>
2023-04-18 17:03:44 +0300

    just forcing a push webhook on github

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,7 +9,7 @@ fun main() {
     val container = document.getElementById("root") ?: error("Couldn't find root container!")
     createRoot(container).render(Fragment.create {
         h1 {
-            +"Welcome to Stuff! Here you can manage your Stuff!... well, you name it! This is a React+Kotlin/JS app!"
+            +"Welcome to Stuff! Here you can manage your Stuff(...LE hmm I might also make it a bit more specific as I have new ideas)!... well, you name it! This is a React+Kotlin/JS app!"
         }
     })
 }
