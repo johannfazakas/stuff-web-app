3ec5603662d31365b758ab6fdbeec8dc4d61938a
Johann <johannfazakas@gmail.com>
2023-04-19 10:50:51 +0300

    changed header message

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,7 +9,7 @@ fun main() {
     val container = document.getElementById("root") ?: error("Couldn't find root container!")
     createRoot(container).render(Fragment.create {
         h1 {
-            +"Welcome! Here you can manage your Stuff(...LE hmm I might also make it a bit more specific as I have new ideas)!... well, you name it! This is a React+Kotlin/JS app!"
+            +"Welcome! This will someday by my Portfolio App! It uses React+Kotlin/JS!"
         }
     })
 }
