0856f87a7dea35bc46cdc76d6b9865ec3586a3ae
Johann <johannfazakas@gmail.com>
2023-04-19 09:14:30 +0300

    small tweaks to play with pipeline

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,11 +5,11 @@ import react.dom.client.createRoot
 import react.dom.html.ReactHTML.h1
 
 fun main() {
-    document.bgColor = "red"
+    document.bgColor = "green"
     val container = document.getElementById("root") ?: error("Couldn't find root container!")
     createRoot(container).render(Fragment.create {
         h1 {
-            +"Welcome to Stuff! Here you can manage your Stuff(...LE hmm I might also make it a bit more specific as I have new ideas)!... well, you name it! This is a React+Kotlin/JS app!"
+            +"Welcome to Stuff! (Or will it be Money Flow?) Here you can manage your Stuff(...LE hmm I might also make it a bit more specific as I have new ideas)!... well, you name it! This is a React+Kotlin/JS app!"
         }
     })
 }
